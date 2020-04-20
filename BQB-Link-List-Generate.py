998e48a07088a6e0e97afcf457a0a546cf5c2f6d
GamerNoTitle <bili33@87ouo.top>
2020-04-20 12:31:50 +0800

    修改PY脚本，便于使用

--- BQB-Link-List-Generate.py
+++ BQB-Link-List-Generate.py
@@ -1,6 +1,6 @@
-import json as js
 import os
-path = 'D:\OneDrive - 87ouo,Inc\Github\Picture-repo-v1\img\BQB'
+localpath = 'D:\OneDrive - 87ouo,Inc\Github\Picture-repo-v1\img\BQB'
+remotepath = 'https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo-v1@master/img/BQB/'
 def findAllFile(base):
     for root, ds, fs in os.walk(base):
         for f in fs:
@@ -11,7 +11,7 @@ def main():
     linklist=[]
     num=1
     for i in findAllFile(base):
-        linklist.append('custom{}: "https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo-v1@master/img/BQB/{}",'.format(num,i))
+        linklist.append('custom{}: '.format(num)+ remotepath + "{}" .format(i))
         num=num+1
     print(str(linklist).replace(',\', \'',', '))
     # print(linklist)
